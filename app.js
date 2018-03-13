1f25b9fe2228cf8c16a87ec372b71e2e0ab21ed9
dreyhard <andrefverratti@gmail.com>
2018-03-12 21:56:21 -0300

    form-base

---
+++ app.js
@@ -0,0 +1,9 @@
+ var config = {
+    apiKey: "AIzaSyB9MGQZJrqmXcTUhbj2O7X7hasFmoyDsHs",
+    authDomain: "bikcraft-78d52.firebaseapp.com",
+    databaseURL: "https://bikcraft-78d52.firebaseio.com",
+    projectId: "bikcraft-78d52",
+    storageBucket: "bikcraft-78d52.appspot.com",
+    messagingSenderId: "1020818667426"
+  };
+  firebase.initializeApp(config);
